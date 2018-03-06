3656be6f0880de6cb3f5370ac4505747fc553c0b
marcpre <marcus.presich@gmail.com>
2018-03-06 14:56:49 +0100

    Change the person component to redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
+import Persons from './containers/Persons';
 import Counter from './containers/Counter/Counter';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Counter />
+        <ol>
+          <li>Turn this app into one which does NOT use local state (in components) but instead uses Redux</li>
+        </ol>
+        <Persons />
       </div>
     );
   }
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,47 +1,28 @@
-import * as actionTypes from './actions'
+import * as actionTypes from './actions';
 
 const initialState = {
-    counter: 0,
-    results: []
-}
+    persons: []
+};
 
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case actionTypes.INCREMENT:
-            // one way to make an inmutable copy of the state
-            const newState = Object.assign({}, state)
-            newState.counter = state.counter + 1
-            return newState
-        case actionTypes.DECREMENT:
-            return {
-                counter: state.counter - 1
+const reducer = ( state = initialState, action ) => {
+    switch ( action.type ) {
+        case actionTypes.ADD_PERSON:
+            const newPerson = {
+                id: Math.random(), // not really unique but good enough here!
+                name: action.personData.name,
+                age: action.personData.age
             }
-        case actionTypes.ADD:
             return {
                 ...state,
-                counter: state.counter + action.val
+                persons: state.persons.concat( newPerson )
             }
-        case actionTypes.SUBSTRACT:
+        case actionTypes.REMOVE_PERSON:
             return {
                 ...state,
-                counter: state.counter - action.val
-            }
-        case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: state.counter})
-            }
-        case actionTypes.DELETE_RESULT:
-            // const id = 2;
-            // const newArray = [...state.results]
-            // newArray.splice(id, 1)
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId)
-            return {
-                ...state,
-                results: updatedArray
+                persons: state.persons.filter(person => person.id !== action.personId)
             }
     }
-    return state
-}
+    return state;
+};
 
-export default reducer
+export default reducer;
